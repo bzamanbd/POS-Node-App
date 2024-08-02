f37f52938c62351701f7fd45bb5b5358d8123f0a
bzamanbd <riponzamanbd@gmail.com>
2024-08-02 23:26:09 +0600

    reports_formatter_ pdf_and_csv_added

--- Desktop/POS-Node-App/reports_module/sales_report/controllers.js
+++ Desktop/POS-Node-App/reports_module/sales_report/controllers.js
@@ -1,5 +1,9 @@
 import prisma from '../../db_client/prisma_client.js';
 
+import PDFDocument from 'pdfkit';
+
+import {Parser} from 'json2csv'
+
 export const totalSalesReport = async(req,res)=>{  
     const shopId = req.salesman.shopId
     try {
@@ -21,6 +25,62 @@ export const totalSalesReport = async(req,res)=>{
 }
 
 
+export const totalSalesReportPdf = async(req,res)=>{  
+    const shopId = req.salesman.shopId
+    try {
+        const totalSales = await prisma.sale.aggregate({ 
+            where:{shopId}, 
+            _sum:{totalPrice:true}
+        });
+
+        const doc = new PDFDocument();
+        let filename = 'total_sales_report.pdf';
+        filename = encodeURIComponent(filename);
+        res.setHeader('Content-disposition', 'attachment; filename="' + filename + '"');
+        res.setHeader('Content-type', 'application/pdf');
+        doc.save().moveTo(0, 0).lineTo(0, 70).lineTo(0, 70).lineTo(50, 0).fill('#FF3300');
+        doc.fontSize(16)
+        doc.fill('#000000')
+        doc.text(`Total Sales: ${totalSales._sum.totalPrice}`,{align:'center'},);
+        doc.text('dhaka,bangladesh',{align:'center'});
+        
+        doc.pipe(res);
+        doc.end();  
+    } catch (e) {
+        return res.status(500).json({ 
+            error:"Something went wrong", 
+            e
+        })
+    }
+    
+}
+
+export const totalSalesReportCsv = async(req,res)=>{  
+    const shopId = req.salesman.shopId
+    try {
+        const totalSales = await prisma.sale.aggregate({ 
+            where:{shopId}, 
+            _sum:{totalPrice:true}
+        });
+        const fields = ['_sum.totalPrice'];
+        const opts = { fields };
+        const parser = new Parser(opts);
+        const csv = parser.parse(totalSales); 
+
+        res.header('Content-Type', 'text/csv');
+        res.attachment('total_sales_report.csv');
+        res.send(csv);
+    } catch (e) {
+        return res.status(500).json({ 
+            error:"Something went wrong", 
+            e
+        })
+    }
+    
+}
+
+
+
 export const salesReportByProduct = async(req,res)=>{  
     const shopId = req.salesman.shopId
     try {
@@ -33,6 +93,69 @@ export const salesReportByProduct = async(req,res)=>{
             message:'Product base sales',
             sales
         });
+    } catch (e) {
+        return res.status(500).json({ 
+            error:"Something went wrong", 
+            e
+        })
+    }
+    
+}
+
+
+
+export const salesReportByProductCsv = async(req,res)=>{  
+    const shopId = req.salesman.shopId
+    try {
+        const sales = await prisma.saleItem.groupBy({
+            where:{shopId},
+            by:['productId'],
+            _sum:{quantity:true, totalPrice:true},
+        })
+
+        const fields = ['productId', '_sum.quantity', '_sum.totalPrice'];
+        const opts = { fields };
+        const parser = new Parser(opts);
+        const csv = parser.parse(sales);
+      
+        res.header('Content-Type', 'text/csv');
+        res.attachment('sales_by_product_report.csv');
+        res.send(csv);
+    } catch (e) {
+        return res.status(500).json({ 
+            error:"Something went wrong", 
+            e
+        })
+    }
+    
+}
+
+export const salesReportByProductPdf = async(req,res)=>{  
+    const shopId = req.salesman.shopId
+    try {
+        const sales = await prisma.saleItem.groupBy({
+            where:{shopId},
+            by:['productId'],
+            _sum:{quantity:true, totalPrice:true},
+        });
+
+        const doc = new PDFDocument();
+        let filename = 'sales_by_product_report.pdf';
+        filename = encodeURIComponent(filename);
+        res.setHeader('Content-disposition', 'attachment; filename="' + filename + '"');
+        res.setHeader('Content-type', 'application/pdf');
+        
+        doc.text('Report of Sales by Product', {align: 'center'});
+        
+        sales.forEach((item) => {
+            doc.text(`Product ID: ${item.productId}`);
+            doc.text(`Quantity Sold: ${item._sum.quantity}`);
+            doc.text(`Total Sales: ${item._sum.totalPrice}`);
+            doc.text('------------------------------------');
+          });
+
+        doc.pipe(res);
+        doc.end();
     } catch (e) {
         return res.status(500).json({ 
             error:"Something went wrong", 
--- Desktop/POS-Node-App/reports_module/sales_report/routes.js
+++ Desktop/POS-Node-App/reports_module/sales_report/routes.js
@@ -1,10 +1,23 @@
 import { Router } from "express";
 import { isLoggedin } from "../../middlewares/isLoggedin.js";
-import { totalSalesReport,salesReportByProduct,salesReportByDate } from "./controllers.js";
+import { 
+    totalSalesReport,
+    salesReportByProduct,
+    salesReportByDate,
+    totalSalesReportPdf,
+    salesReportByProductPdf,
+    totalSalesReportCsv,
+    salesReportByProductCsv
+} from "./controllers.js";
+
 const routes = Router() 
 
 routes.get("/sales/total",isLoggedin,totalSalesReport) 
+routes.get("/sales/total/pdf",isLoggedin,totalSalesReportPdf) 
+routes.get("/sales/total/csv",isLoggedin,totalSalesReportCsv) 
 routes.get("/sales/by-product",isLoggedin,salesReportByProduct) 
+routes.get("/sales/by-product/pdf",isLoggedin,salesReportByProductPdf) 
+routes.get("/sales/by-product/csv",isLoggedin,salesReportByProductCsv) 
 routes.get("/sales/by-date",isLoggedin,salesReportByDate) 
  
 
